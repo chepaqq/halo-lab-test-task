86ab2cf122138d59e618254f1e62d751ef8149e4
chepaqq <chepaqq@protonmail.com>
2023-07-16 16:28:13 +0300

    fix: Check error return value of Run function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,5 +13,7 @@ func main() {
 			"message": "Hello, world!",
 		})
 	})
-	r.Run()
+	if err := r.Run(); err != nil {
+		log.Fatalln(err.Error())
+	}
 }
