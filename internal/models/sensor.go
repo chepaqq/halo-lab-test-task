1dc44240a2a4eab65b90e1411515c1f69cdedd8b
chepaqq <chepaqq@protonmail.com>
2023-07-18 11:06:51 +0300

    fix: Add missing fields to models

--- internal/models/sensor.go
+++ internal/models/sensor.go
@@ -2,30 +2,43 @@ package models
 
 import "time"
 
+// Coordinates -.
 type Coordinates struct {
 	X int `json:"x,omitempty"`
 	Y int `json:"y,omitempty"`
 	Z int `json:"z,omitempty"`
 }
 
+// Sensor - .
 type Sensor struct {
-	ID             int         `json:"id,omitempty"`
-	Codename       string      `json:"codename,omitempty"`
+	Index          int         `json:"id,omitempty"`
+	Group          string      `json:"group,omitempty"`
 	Coordinates    Coordinates `json:"coordinates,omitempty"`
 	DataOutputRate int         `json:"data_output_rate,omitempty"`
-	Group          string      `json:"group,omitempty"`
 }
 
+// Fish - .
 type Fish struct {
 	ID   int    `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
 }
 
+// DetectedFishes - .
+type DetectedFishes struct {
+	ID          int    `json:"id,omitempty"`
+	SensorIndex int    `json:"sensor_index,omitempty"`
+	SensorGroup string `json:"sensor_group,omitempty"`
+	FishID      int    `json:"fish_id,omitempty"`
+	Count       int    `json:"count,omitempty"`
+}
+
+// SensorsDetection - .
 type SensorsDetection struct {
-	ID             int            `json:"id,omitempty"`
-	SensorID       int            `json:"sensor_id,omitempty"`
-	Temperature    float64        `json:"temperature,omitempty"`
-	Transparency   int            `json:"transparency,omitempty"`
-	DetectedFishes map[string]int `json:"detected_fishes,omitempty"`
-	DetectionTime  time.Time      `json:"detection_time,omitempty"`
+	ID               int       `json:"id,omitempty"`
+	SensorIndex      int       `json:"sensor_id,omitempty"`
+	SensorGroup      string    `json:"sensor_group,omitempty"`
+	DetectedFishesID int       `json:"detected_fishes_id,omitempty"`
+	Temperature      float64   `json:"temperature,omitempty"`
+	Transparency     int       `json:"transparency,omitempty"`
+	DetectedAt       time.Time `json:"detection_time,omitempty"`
 }
