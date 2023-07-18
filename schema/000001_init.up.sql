2724e2311649f19fee37fe29a8b8f1426c1bf578
chepaqq <chepaqq@protonmail.com>
2023-07-18 10:54:30 +0300

    fix: Add missing primary keys and fields in database

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,7 +1,14 @@
 CREATE TABLE
+  fish (
+    id serial primary key not null unique,
+    name varchar(255) not null
+  );
+
+CREATE TABLE
   sensor (
-    id serial not null unique,
-    codename varchar(255) not null,
+    index serial not null,
+    group varchar(40) not null,
+    primary key (index, group),
     x int not null,
     y int not null,
     z int not null,
@@ -9,25 +16,23 @@ CREATE TABLE
   );
 
 CREATE TABLE
-  fish (
-    id serial not null unique,
-    name varchar(255) not null
-  );
-
-CREATE TABLE
   detected_fishes (
     id serial not null unique,
-    sensor_id int not null,
+    sensor_index int not null,
+    sensor_group varchar(40) not null,
+    foreign key (sensor_index, sensor_group) references sensor (index, group) on delete cascade not null,
     fish_id int not null,
     count int not null
   );
 
 CREATE TABLE
   sensor_detection (
-    id serial not null unique,
-    sensor_id int references sensor (id) on delete cascade not null,
+    id serial primary key not null unique,
+    sensor_index int not null,
+    sensor_group varchar(40) not null,
+    foreign key (sensor_index, sensor_group) references sensor (index, group) on delete cascade not null,
     detected_fishes_id int references detected_fishes (int) on delete cascade not null,
     temperature real not null,
     transparency int not null,
-    detection_time time not null
+    detected_at time not null
   );
