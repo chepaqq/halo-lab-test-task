1834b64923c8bcc6dc260bf2473426f462e9d0cd
chepaqq <chepaqq@protonmail.com>
2023-07-20 22:00:19 +0300

    feat: Add generating data in goroutine in main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,10 +5,17 @@ import (
 	"os"
 
 	"github.com/chepaqq99/halo-lab-test-task/internal/api/handler"
+	"github.com/chepaqq99/halo-lab-test-task/internal/generation"
 	"github.com/chepaqq99/halo-lab-test-task/pkg/httpserver"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	go generation.InitGeneration()
 	srv := new(httpserver.Server)
 	if err := srv.Run(os.Getenv("PORT"), handler.InitRoutes()); err != nil {
 		log.Fatalln(err.Error())
